eaa6012cb0c307b99a1fadcc96abc241428360db
elenacalugaru <53155248+elenacalugaru@users.noreply.github.com>
2022-11-20 09:08:13 +0200

    end od section 2 datingapp

---
+++ datingapp.session.sql
@@ -0,0 +1,8 @@
+INSERT INTO Users (Id, UserName)
+VALUES (1, 'Bob');
+INSERT INTO Users (Id, UserName)
+VALUES (2, 'Tom');
+INSERT INTO Users (Id, UserName)
+VALUES (3, 'Jane');
+INSERT INTO Users (Id, UserName)
+VALUES (4, 'Elena');
