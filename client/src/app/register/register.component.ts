eb544c1f8b022b50ba650a8ca879d8988a43b236
elenacalugaru <53155248+elenacalugaru@users.noreply.github.com>
2022-12-06 00:24:42 +0200

    end of section 6 routing guards in angular

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 
@@ -13,7 +14,7 @@ export class RegisterComponent implements OnInit {
   model : any = {}
 
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr : ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -23,13 +24,11 @@ export class RegisterComponent implements OnInit {
     this.accountService.register(this.model).subscribe
     (
       {
-          next : () => {
-            this.cancel();
-          },
-          error: error => {
+          next : _ =>  this.cancel(),
+          error: error =>  {
+            this.toastr.error(error.error)
             console.log(error)
-          } 
-          
+          }
       }
 
     );
